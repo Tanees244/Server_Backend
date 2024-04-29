105feaada0f74c67ecb06fd97e06ebf6cb0cfcf3
Tanees <k201879@nu.edu.pk>
2024-04-30 00:11:15 +0500

    Tanees Commit

--- GuideRoutes.js
+++ GuideRoutes.js
@@ -64,6 +64,69 @@ GuideRouter.get('/guide-details', async (req, res) => {
   }
 });
 
+GuideRouter.put('/update-user-rating', async (req, res) => {
+  const authToken = req.headers.authorization;
+  
+  if (!authToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const token = authToken.split(' ')[1];
+    const decodedToken = await verifyAsync(token, secretKey);
+
+    if (!decodedToken) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    const userId = decodedToken.userId;
+
+    // Extract userRating from the request body
+    const { userRating, packageId } = req.body;
+
+    const guideIdQuery = 'SELECT guide_id FROM guides WHERE user_id = ?';
+
+    pool.query(guideIdQuery, [userId], async (error, guideIdResults) => {
+      if (error) {
+        console.error('Error fetching guide ID:', error);
+        return res.status(500).json({ error: 'Error fetching guide ID' });
+      }
+
+      if (guideIdResults.length === 0) {
+        return res.status(404).json({ error: 'Guide not found' });
+      }
+
+      const guideId = guideIdResults[0].guide_id;
+
+      // Update the user_rating in the guide_operations table
+      const updateUserRatingQuery = `
+        UPDATE guide_operations
+        SET user_rating = ?
+        WHERE guide_id = ? AND package_id = ?
+      `;
+
+      pool.query(updateUserRatingQuery, [userRating, guideId, packageId], (err, updateResults) => {
+        if (err) {
+          console.error('Error updating user rating:', err);
+          return res.status(500).json({ error: 'Error updating user rating' });
+        }
+
+        // Check if any rows were affected
+        if (updateResults.affectedRows === 0) {
+          return res.status(404).json({ error: 'User rating not updated' });
+        }
+
+        // User rating successfully updated
+        res.status(200).json({ message: 'User rating updated successfully' });
+      });
+    });
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
 GuideRouter.get('/guide-packages', async (req, res) => {
   const authToken = req.headers.authorization;
   
@@ -303,11 +366,11 @@ GuideRouter.get("/guide_applications", async (req, res) => {
     console.log(status);
 
     if (status === "Active") {
-      query = `SELECT ga.*, gd.name AS guide_name, gd.picture AS guide_picture FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'active'`;
+      query = `SELECT ga.*, gd.name AS guide_name, gd.picture AS guide_picture, gd.cnic_front_picture as cfp, gd.cnic_back_picture as cbp, gd.guide_license_picture as glp FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'active'`;
     } else if (status === "Pending") {
-      query = `SELECT ga.*, gd.name AS guide_name , gd.picture AS guide_picture FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'pending'`;
+      query = `SELECT ga.*, gd.name AS guide_name, gd.picture AS guide_picture, gd.cnic_front_picture as cfp, gd.cnic_back_picture as cbp, gd.guide_license_picture as glp FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'pending'`;
     } else if (status === "Rejected") {
-      query = `SELECT ga.*, gd.name AS guide_name , gd.picture  AS guide_picture FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'rejected'`;
+      query = `SELECT ga.*, gd.name AS guide_name, gd.picture AS guide_picture, gd.cnic_front_picture as cfp, gd.cnic_back_picture as cbp, gd.guide_license_picture as glp FROM guide_application AS ga INNER JOIN guide_personal_details AS gd ON ga.guide_id = gd.guide_id WHERE ga.status = 'rejected'`;
     } else {
       return res.status(400).json({ error: "Invalid status parameter" });
     }
@@ -317,16 +380,21 @@ GuideRouter.get("/guide_applications", async (req, res) => {
         console.error("Error fetching guide applications:", error);
         return res.status(500).json({ error: "Error fetching data" });
       }
-
       const usersObject = results.reduce((acc, user) => {
         acc[user.guide_id] = {
           ...user,
           guide_name: user.guide_name,
-          guide_picture: user.guide_picture 
+          guide_picture: user.guide_picture ? Buffer.from(user.guide_picture).toString("base64") : null,
+          cfp: user.cfp ? Buffer.from(user.cfp).toString("base64") : null,
+          cbp: user.cbp ? Buffer.from(user.cbp).toString("base64") : null,
+          glp: user.glp ? Buffer.from(user.glp).toString("base64") : null,
         };
         return acc;
       }, {});
 
+
+
+console.log(usersObject);
       res.status(200).json(usersObject);
     });
   } catch (error) {
